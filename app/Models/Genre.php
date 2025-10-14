54899d6bf26f85d3ad2b753858d4987979c249a3
araisyaanfl <araisyaanfl@gmail.com>
2025-10-14 15:54:11 +0700

    Tugas 1 Laravel - Alika Raisya Universitas Muhammadiyah Jakarta

---
+++ app/Models/Genre.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Genre extends Model
+{
+    private $genres = [
+        [
+            'id' => 1,
+            'name' => 'Drama',
+            'description' => 'Cerita yang menggambarkan kehidupan dan konflik emosional manusia.'
+        ],
+        [
+            'id' => 2,
+            'name' => 'Detektif',
+            'description' => 'Genre yang berfokus pada pemecahan kasus dan misteri kriminal.'
+        ],
+        [
+            'id' => 3,
+            'name' => 'Misteri Sekolah',
+            'description' => 'Kisah misteri yang berlatar di lingkungan sekolah.'
+        ],
+        [
+            'id' => 4,
+            'name' => 'Fiksi Ilmiah',
+            'description' => 'Cerita berlatar dunia futuristik atau eksperimen ilmiah dengan elemen petualangan.'
+        ],
+        [
+            'id' => 5,
+            'name' => 'Thriller Psikologis',
+            'description' => 'Genre dengan ketegangan mental dan emosi yang intens, sering berfokus pada misteri atau kejahatan.'
+        ]
+    ];
+
+    public function getGenres() {
+        return $this->genres;
+    }
+}
