e95a664722cd3ea2f9fa4fcf0820a5e6143fdac9
araisyaanfl <araisyaanfl@gmail.com>
2025-10-20 20:49:49 +0700

    Tugas 6 Laravel - Alika Raisya Universitas Muhammadiyah Jakarta

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\GenreController;
-use App\Models\Genre;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,9 +11,29 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::apiResource('/books', BookController::class);
+
+Route::apiResource('/books', BookController::class)->only('index', 'show');
+Route::apiResource('/genres', GenreController::class)->only('index', 'show');
+Route::apiResource('/authors', AuthorController::class)->only('index', 'show');
+
+
+Route::middleware(['auth:api'])->group(function () {
+
+    Route::middleware(['role:admin'])->group(function () {
+        Route::apiResource('/books', BookController::class)->only(['store','update','destroy']);
+        Route::apiResource('/authors', AuthorController::class)->only(['store','update', 'destroy']);
+        Route::apiResource('/genres', AuthorController::class)->only(['store','update', 'destroy']);
+    });
+
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+
+
 
 // Route::get('/books', [BookController::class, 'index']);
 // Route::post('/books', [BookController::class, 'store']);
@@ -23,11 +43,12 @@ Route::apiResource('/books', BookController::class);
 
 
 
-Route::apiResource('/genres', GenreController::class);
 // Route::get('/genres', [GenreController::class, 'index']);
 // Route::post('/genres', [GenreController::class, 'store']);
 
 
-Route::apiResource('/authors', AuthorController::class);
+
 // Route::get('/authors',[AuthorController::class, 'index']);
 // Route::post('/authors',[AuthorController::class, 'store']);
+
+
